c19f7b12fd451cbf5200f1deff34e4974ebf20ad
Tiago Guerra <a21705067@alunos.ulusofona.pt>
2021-05-30 19:15:19 +0100

    profile

---
+++ src/app/camera/camera.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from "../services/photo.service";
+
+@Component({
+  selector: 'app-camera',
+  templateUrl: './camera.page.html',
+  styleUrls: ['./camera.page.scss'],
+})
+export class CameraPage implements OnInit {
+
+  constructor(public PhotoService: PhotoService) { }
+
+  ngOnInit() {
+  }
+
+  takePicture(){
+    this.PhotoService.takePicture();
+  }
+
+}
